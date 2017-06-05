5e7a98769a7666fe2725c9e09c9fef6010c003f6
Eumir Gaspar <eumirg@rent.com.au>
2017-06-06 03:48:46 +0800

    Initial dirty long solution

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -7,7 +7,30 @@ defmodule RotationalCipher do
   "Nggnpx ng qnja"
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
-  def rotate(text, shift) do
+  def rotate(<<text::binary-size(1)>>, shift) do
+    text |> String.to_charlist
+         |> List.first
+         |> compute(shift)
+         |> List.wrap
+         |> List.to_string
+  end
+  def rotate(<<head::binary-size(1), tail::binary>>, shift) do
+    kek = head |> String.to_charlist
+         |> List.first
+         |> compute(shift)
+
+    [kek, rotate(tail, shift)] |> List.to_string
+  end
+
+  def compute(num, shift) when ((num >= ?a) and (num <= ?z) and (num+shift) > ?z) or
+                               ((num >= ?A) and (num <= ?Z) and (num+shift) > ?Z) do
+    (num + shift) |> Kernel.-(26) |> Kernel.abs
+  end
+  def compute(num, shift) when num == ?\s or (num <= ?9) do
+    num
+  end
+  def compute(num, shift) do
+    num + shift
   end
 end
 
