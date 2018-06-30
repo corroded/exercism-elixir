42aa1b8f472692794d48e9e6381ebe049eabf639
Eumir Gaspar <eumirg@rent.com.au>
2018-07-01 07:49:42 +0800

    Went straight for map instead

--- nucleotide-count/nucleotide_count.exs
+++ nucleotide-count/nucleotide_count.exs
@@ -28,6 +28,6 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
-    for {char, val} <- %{?A => 0, ?T => 0, ?C => 0, ?G => 0}, into: %{}, do: { char, count(strand, char) }
+    @nucleotides |> Map.new(fn(x) -> { x, count(strand, x) } end)
   end
 end
