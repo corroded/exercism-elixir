27d625a7804b7d66de5c4af5f19c8b56c876cc81
Eumir Gaspar <eumirg@rent.com.au>
2018-07-01 07:22:37 +0800

    Fixed typo in  values

--- nucleotide-count/nucleotide_count.exs
+++ nucleotide-count/nucleotide_count.exs
@@ -28,6 +28,6 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
-    for {char, val} <- %{?A => 0, ?T => 0, ?C => 0, ?G => 8}, into: %{}, do: { char, count(strand, char) }
+    for {char, val} <- %{?A => 0, ?T => 0, ?C => 0, ?G => 0}, into: %{}, do: { char, count(strand, char) }
   end
 end
